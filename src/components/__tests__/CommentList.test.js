6544c7a6e7a6f38c661c3b24b6713cdb3d653a13
Nicholas Smith <npsmith1990@gmail.com>
2018-06-16 16:02:42 -0600

    Make tests pass

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -3,13 +3,14 @@ import { mount } from 'enzyme'
 
 import CommentList from 'components/CommentList'
 import Root from 'Root'
-import * as actions from 'actions'
 
 let wrapper
+let testComment1 = 'test comment 1'
+let testComment2 = 'test comment 2'
 
 beforeEach(() => {
   const initialState = {
-    comments: [ "test comment 1", "test comment 2"]
+    comments: [ testComment1, testComment2 ]
   }
   wrapper = mount(
     <Root initialState={initialState}>
@@ -22,7 +23,7 @@ it('should render one <li/> per comment', () => {
   expect(wrapper.find('li').length).toEqual(2)
 })
 
-// it('should render the actual comment in the <li/>', () => {
-//   const testComment = "Test comment"
-//   actions.saveComment(testComment)
-// })
+it('should render the actual comment in the <li/>', () => {
+  expect(wrapper.text()).toContain(testComment1)
+  expect(wrapper.text()).toContain(testComment2)
+})
