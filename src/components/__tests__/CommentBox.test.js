b47f79e767e5a7ed94b19e6f8d32ab589bd4b4ee
Nicholas Smith <npsmith1990@gmail.com>
2018-06-16 10:52:54 -0600

    Refactor

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,34 +17,28 @@ it('shows a textarea and a button', () => {
   expect(wrapper.find('textarea').length).toEqual(1)
   expect(wrapper.find('button').length).toEqual(1)
 })
+describe('textarea', () => {
+  let testComment = 'Test comment'
 
-it('allows a user to enter text in textarea', () => {
-  const event = {
-    target: {
-      value: 'Test comment'
+  beforeEach(() => {
+    const event = {
+      target: {
+        value: testComment
+      }
     }
-  }
-  wrapper.find('textarea').simulate('change', event)
-  // force re-render (does not detect state change)
-  wrapper.update()
-  
-  expect(wrapper.find('textarea').prop('value')).toEqual('Test comment')
-})
+    wrapper.find('textarea').simulate('change', event)
+    wrapper.update() // force re-render (jsdom does not render upon state change)
+  })
 
-it('empties the text area when form is submitted', () => {
-  const event = {
-    target: {
-      value: 'Test comment'
-    }
-  }
-  wrapper.find('textarea').simulate('change', event)
-  // force re-render (does not detect state change)
-  wrapper.update()
-    
-  expect(wrapper.find('textarea').prop('value')).toEqual('Test comment')
+  it('allows a user to enter text in textarea', () => {
+    expect(wrapper.find('textarea').prop('value')).toEqual(testComment)
+  })
   
-  wrapper.find('form').simulate('submit')
+  it('empties the text area when form is submitted', () => {
+    expect(wrapper.find('textarea').prop('value')).toEqual(testComment)
+    wrapper.find('form').simulate('submit')
+    wrapper.update()
+    expect(wrapper.find('textarea').prop('value')).toEqual('')
+  })
   
-  wrapper.update()
-  expect(wrapper.find('textarea').prop('value')).toEqual('')
 })
